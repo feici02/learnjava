f3330bec937571a16a024b5f8d0143ee54603ede
WANG Xu <feici02@163.com>
2019-10-01 10:41:45 +0800

    initial commit

---
+++ src/main/java/jodatime/App.java
@@ -0,0 +1,10 @@
+package jodatime;
+
+import org.joda.time.LocalTime;
+
+public class App {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+    }
+}
