c5a4733affdcf9daa133a8c8881154ff1d13c348
WANG Xu <feici02@163.com>
2019-10-13 21:28:11 +0800

    add a generic sample

---
+++ src/main/java/generics/MyGeneric.java
@@ -0,0 +1,15 @@
+package generics;
+
+import java.util.List;
+
+public class MyGeneric<T> {
+    private List<T> list;
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
